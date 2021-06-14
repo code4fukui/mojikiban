595dc5f79aeba85ce4ac596605c4e3ebe20bd9c9
Taisuke Fukuno <fukuno@jig.jp>
2021-06-15 04:27:20 +0900

    support '+' separator

--- Moji.js
+++ Moji.js
@@ -9,9 +9,25 @@ const mj2imglink = (mj, base = "https://moji.or.jp/mojikibansearch/img/MJ/", ext
 };
 
 const ucs2s = (ucs) => {
+  if (ucs.startsWith("U+")) {
+    ucs = ucs.substring(2);
+  }
   const n = ucs.indexOf("_");
   if (n < 0) {
-    return String.fromCodePoint(parseInt(ucs, 16));
+    const n = ucs.indexOf(" U+");
+    if (n < 0) {
+      const n = ucs.indexOf("+");
+      if (n < 0) {
+        return String.fromCodePoint(parseInt(ucs, 16));
+      } else {
+        const base = parseInt(ucs.substring(0, n), 16);
+        const e = parseInt(ucs.substring(n + 1), 16);
+        return String.fromCodePoint(base, e);
+      }
+    }
+    const base = parseInt(ucs.substring(0, n), 16);
+    const e = parseInt(ucs.substring(n + 3), 16);
+    return String.fromCodePoint(base, e);
   }
   const base = parseInt(ucs.substring(0, n), 16);
   const e = parseInt(ucs.substring(n + 1), 16);
