351e37bedb1d7a345e531236d6feca72103688e7
Taisuke Fukuno <fukuno@jig.jp>
2021-10-14 06:41:28 +0900

    add ZenkakuAlpha

---
+++ ZenkakuAlpha.js
@@ -0,0 +1,67 @@
+class ZenkakuAlpha {
+  static isZen(c) {
+    if (c == null || c == "") {
+      return false;
+    }
+    const n = c.codePointAt();
+    return n >= 65281 && n <= 65374;// || n == 12288; // not include zen space
+  }
+  static isHan(c) {
+    if (c == null || c == "") {
+      return false;
+    }
+    const n = c.codePointAt();
+    return n >= 33 && n <= 126;
+  }
+  static _convert(s, from, to) {
+    if (s == null) {
+      return s;
+    }
+    const res = [];
+    for (const c of s) {
+      const n = from.indexOf(c)
+      if (n >= 0) {
+        res.push(to[n]);
+      } else {
+        res.push(c);
+      }
+    }
+    return res.join("");
+  }
+  static toHan(s) {
+    if (s == null) {
+      return s;
+    }
+    const res = [];
+    for (const c of s) {
+      const n = c.codePointAt(0);
+      if (n >= 65281 && n <= 65374) {
+        res.push(String.fromCodePoint(n - 65248));
+      //} else if (n == 12288) {
+      //  res.push(" ");
+      } else {
+        res.push(c);
+      }
+    }
+    return res.join("");
+  }
+  static toZen(s) {
+    if (s == null) {
+      return s;
+    }
+    const res = [];
+    for (const c of s) {
+      const n = c.codePointAt(0);
+      if (n >= 33 && n <= 126) {
+        res.push(String.fromCodePoint(n + 65248));
+      //} else if (n == 32) {
+      //  res.push("　");
+      } else {
+        res.push(c);
+      }
+    }
+    return res.join("");
+  }
+}
+
+export { ZenkakuAlpha };
---
+++ test/ZenkakuAlpha.test.js
@@ -0,0 +1,44 @@
+import * as t from "https://deno.land/std/testing/asserts.ts";
+import { ZenkakuAlpha } from "../ZenkakuAlpha.js";
+
+const zenall = "！＂＃＄％＆＇（）＊＋，－．／０１２３４５６７８９：；＜＝＞？＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ［＼］＾＿｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ｛｜｝～";
+const hanall = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
+/*
+// make zenall
+const res = [];
+for (let i = 65281; i <= 65374; i++) {
+  res.push(String.fromCodePoint(i));
+}
+console.log(res.join(""), res.length);
+*/
+
+/*
+// make hanall
+const res2 = [];
+for (let i = 33; i <= 126; i++) {
+  res2.push(String.fromCodePoint(i));
+}
+console.log(res2.join(""), res2.length);
+
+console.log(hanall.length, zenall.length); // 95
+*/
+
+Deno.test("toZen", () => {
+  t.assertEquals(ZenkakuAlpha.toZen("abc!"), "ａｂｃ！");
+  t.assertEquals(ZenkakuAlpha.toZen("z$"), "ｚ＄");
+  t.assertEquals(ZenkakuAlpha.toZen(hanall), zenall);
+});
+Deno.test("toHan", () => {
+  t.assertEquals(ZenkakuAlpha.toHan("ｚ＄"), "z$");
+  t.assertEquals(ZenkakuAlpha.toHan("z$"), "z$");
+  t.assertEquals(ZenkakuAlpha.toHan(zenall), hanall);
+});
+Deno.test("isZen", () => {
+  t.assert(ZenkakuAlpha.isZen("ｚ"));
+  t.assert(!ZenkakuAlpha.isZen("z"));
+});
+Deno.test("space", () => {
+  t.assertEquals(ZenkakuAlpha.toZen(" "), " "); // han spc -> han spc (not convert)
+  t.assertEquals(ZenkakuAlpha.toHan("　"), "　"); // zen spc -> zen spc (not convert)
+});
